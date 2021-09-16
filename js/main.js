cb69f2d16b386d9d2d2fbcbd3ae1247f5234f34e
jsygo <joshsygobusiness@gmail.com>
2021-09-16 22:10:46 +0000

    created data model, added event listener for keydown, in process of creating css classes for turning

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
 /* global data */
 /* exported data */
+
+var $car = document.querySelector('.car');
+
+window.addEventListener('keydown', function () {
+  if (event.key === 'ArrowDown') {
+    $car.setAttribute('class', 'car down');
+    data.currentDirection = 'down';
+  }
+  if (event.key === 'ArrowLeft') {
+    $car.setAttribute('class', 'car left');
+    data.currentDirection = 'left';
+  }
+  if (event.key === 'ArrowRight') {
+    if (data.currentDirection === 'up') {
+      $car.setAttribute('class', 'car right-from-up');
+    } else {
+      $car.setAttribute('class', 'car right');
+    }
+    data.currentDirection = 'right';
+  }
+  if (event.key === 'ArrowUp') {
+    if (data.currentDirection === 'right') {
+      $car.setAttribute('class', 'car up-from-right');
+    } else {
+      $car.setAttribute('class', 'car up');
+    }
+    data.currentDirection = 'up';
+  }
+})
+;
