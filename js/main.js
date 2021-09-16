ae15a58d6c0b5b1eebcd704bd919dba6aa00e553
jsygo <joshsygobusiness@gmail.com>
2021-09-16 23:38:25 +0000

    If user presses space, make the car move forward

--- js/main.js
+++ js/main.js
@@ -2,8 +2,15 @@
 /* exported data */
 
 var $car = document.querySelector('.car');
+var $carStyle = $car.style;
 
-window.addEventListener('keydown', function () {
+function moveCar() {
+  console.log('move car!');
+  $carStyle.left = `${data.location.x + 5}px`;
+  data.location.x += 5;
+}
+
+document.addEventListener('keydown', function () {
   if (event.key === 'ArrowDown') {
     $car.setAttribute('class', 'car down');
     data.currentDirection = 'down';
@@ -19,6 +26,9 @@ window.addEventListener('keydown', function () {
   if (event.key === 'ArrowUp') {
     $car.setAttribute('class', 'car up');
     data.currentDirection = 'up';
+  }
+  if (event.key === ' ') {
+    var intervalId = setInterval(moveCar, 16);
   }
 })
 ;
